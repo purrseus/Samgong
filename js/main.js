4b9e5f7989b3b783805e1536438bc8f10a343434
thiendo261 <thiendo3831@gmail.com>
2020-07-02 10:48:33 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const scrollBtn = document.querySelector('.scroll-to-top');
+const mainMenu = document.querySelector('.main-menu');
+const menuBtn = document.querySelector('.res-menu');
+const quitBtn = document.querySelector('.quit');
+
 scrollBtn.addEventListener('click', topFunction);
+menuBtn.addEventListener('click', responsiveMenu);
+quitBtn.addEventListener('click', hideMenu);
 
 function topFunction() {
     document.body.scrollTop = 0;
@@ -12,4 +18,16 @@ window.onscroll = function scrollFunction() {
     } else {
         scrollBtn.style.display = 'none';
     }
+}
+
+function responsiveMenu() {
+    mainMenu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    quitBtn.style.display = 'block';
+}
+
+function hideMenu() {
+    mainMenu.style.display = 'none';
+    menuBtn.style.display = 'block';
+    quitBtn.style.display = 'none';
 }
